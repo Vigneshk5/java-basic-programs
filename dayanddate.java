d0e1982a2ed5e44b9c56ffa5086b12cb4bf53be8
Vignesh K <97999742+Vigneshk5@users.noreply.github.com>
2023-03-09 15:40:35 +0000

    New program

---
+++ dayanddate.java
@@ -0,0 +1,9 @@
+import java.util.Date;  
+  
+public class JavaDateGetDayExample1 {  
+  
+    public static void main(String[] args) {  
+        Date d=new Date();  
+        System.out.println("day of the week is  : "+d.getDay());  
+        }  
+}  
